9519841fa0cbf7a8ed6822ea1fa21c1e092acc38
Harshit sharma <programming966051@gmail.com>
2024-10-24 23:14:55 +0530

    add some file for set up code

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const axios = require('axios')
+const bodyparser = require('body-parser');
+
+
 app.listen(3000 ,()=>{
     console.log('app listen at port number 3000')
 })
